7a6623e02fd042f4d503555f4c5afcd71d6fcecc
erza <58721335+erzaffasya@users.noreply.github.com>
2022-06-20 17:24:11 +0800

    Update sidebar.blade.php

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -13,17 +13,17 @@
             <a class="nav-link collapsed" data-bs-target="#Layanan-nav" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-menu-button-wide"></i><span>Layanan</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="Layanan-nav" class="nav-content {{ request()->is('Layanan*') ? 'show' : '' }} collapse "
+            <ul id="Layanan-nav" class="nav-content {{ request()->is('admin/Layanan*') ? 'show' : '' }} collapse "
                 data-bs-parent="#sidebar-nav">
                 <li>
                     <a href="{{ route('Layanan.create') }}"
-                        class="{{ request()->is('Layanan/create') ? 'active' : '' }}">
+                        class="{{ request()->is('admin/Layanan/create') ? 'active' : '' }}">
                         <i class="bi bi-circle"></i><span>Tambah Layanan</span>
                     </a>
                 </li>
                 <li>
                     <a href="{{ route('Layanan.index') }}"
-                        class="{{ request()->is('Layanan') ? 'active' : '' }}">
+                        class="{{ request()->is('admin/Layanan') ? 'active' : '' }}">
                         <i class="bi bi-circle"></i><span>Data Layanan</span>
                     </a>
                 </li>
@@ -34,17 +34,17 @@
             <a class="nav-link collapsed" data-bs-target="#Informasi-nav" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-menu-button-wide"></i><span>Informasi</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="Informasi-nav" class="nav-content {{ request()->is('Informasi*') ? 'show' : '' }} collapse "
+            <ul id="Informasi-nav" class="nav-content {{ request()->is('admin/Informasi*') ? 'show' : '' }} collapse "
                 data-bs-parent="#sidebar-nav">
                 <li>
                     <a href="{{ route('Informasi.create') }}"
-                        class="{{ request()->is('Informasi/create') ? 'active' : '' }}">
+                        class="{{ request()->is('admin/Informasi/create') ? 'active' : '' }}">
                         <i class="bi bi-circle"></i><span>Tambah Informasi</span>
                     </a>
                 </li>
                 <li>
                     <a href="{{ route('Informasi.index') }}"
-                        class="{{ request()->is('Informasi') ? 'active' : '' }}">
+                        class="{{ request()->is('admin/Informasi') ? 'active' : '' }}">
                         <i class="bi bi-circle"></i><span>Data Informasi</span>
                     </a>
                 </li>
